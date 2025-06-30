016aa7b2321548573794fe794554ee63beca7a95
Hector Sanchez <sanchez.hector.m@gmail.com>
2025-06-30 01:19:28 -0400

    Modularizes entire application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { TaskComponent } from './task/task.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, TasksComponent, UserComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
